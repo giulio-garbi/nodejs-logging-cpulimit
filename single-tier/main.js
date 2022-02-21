65d9c57c5bbcaf7eef164511e27ccc9e4799df91
Giulio Garbi <julius.grb@gmail.com>
2022-02-21 11:36:57 +0100

    Slowed down single tier

--- single-tier/main.js
+++ single-tier/main.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const logger = require('morgan');
 const port = 8080;
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
   
 const app = express();
 app.use(logger(':method :url - rt :response-time ms - exit :date[iso]'));
   
-app.get('/', (req, res) => {
-  res.send('<h1>Front Page</h1>');
+app.get('/', async (req, res) => {
+	await sleep(1000);
+	res.send('<h1>Front Page</h1>');
 });
   
 app.listen(port);
