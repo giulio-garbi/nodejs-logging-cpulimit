4d7130c0607994fbcc206b382ef7ac414a26049f
Giulio Garbi <julius.grb@gmail.com>
2022-02-21 12:11:39 +0100

    Single tier saves to logfile

--- single-tier/main.js
+++ single-tier/main.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const logger = require('morgan');
+const fs = require("fs");
+
 const port = 8080;
+
+const logFname = "/tmp/log.txt"
 
 function sleep(ms) {
   return new Promise((resolve) => {
@@ -8,8 +12,10 @@ function sleep(ms) {
   });
 }
   
+var accessLogStream = fs.createWriteStream(logFname, { flags: 'a' })
+
 const app = express();
-app.use(logger(':method :url - rt :response-time ms - exit :date[iso]'));
+app.use(logger(':method :url - rt :response-time ms - exit :date[iso]', { stream: accessLogStream }));
   
 app.get('/', async (req, res) => {
 	await sleep(1000);
